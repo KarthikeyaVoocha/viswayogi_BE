293a30436c8c2ac2383493a5e442f0e1f2adc3fd
KarthikeyaV-amrrtechsols <karthikv@amrrtechsols.com>
2025-03-02 21:53:23 +0530

    changed the error

--- viswayogi_BE/doctor/views_book_appointment.py
+++ viswayogi_BE/doctor/views_book_appointment.py
@@ -123,9 +123,9 @@ class BookAppointmentView(APIView):
             appointment = serializer.save()
 
             response_data = {
-                "appointment_id": appointment.appointment_id,
-                "patient_id": appointment.patient_id,
-                "doctor_id": appointment.user_id,
+                # "appointment_id": appointment.appointment_id,
+                # "patient_id": appointment.patient_id,
+                # "doctor_id": appointment.user_id,
                 "blood_pressure": appointment.blood_pressure,
                 "weight": appointment.weight,
                 "body_temp": appointment.body_temp,
